5c83246c16acdd46c3df698b3be11cf9c4ad5819
stevorated <garbers8@gmail.com>
2020-02-24 15:25:21 +0200

    Fix url-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './App.scss'
 import PageInterface from './interfaces/PageInterface';
+import img from './assets/img/cuttree.jpg'
 
 class App extends React.Component<PageInterface, {}> {
   render() {
@@ -10,7 +11,7 @@ class App extends React.Component<PageInterface, {}> {
         <h1 className={`header text-${color} text-uppercase display-4`}>
           React-Typescript
         </h1>
-        <img src="/src/assets/img/cuttree.jpg" className="my-5" alt="sada"/>
+        <img src={img} className="my-5" alt="sada"/>
         <p className="mt-3 secondaryHeader">The color of this page is: {color}</p>
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
           {
             loader: "url-loader",
             options: {
-              limit: 8192
+              limit: false
             }
           }
         ]
